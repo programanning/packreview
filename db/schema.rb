83d2af80f8cbb44d2f4eb464b5a5d739d9fc2feb
nunya <austin@dringo.io>
2017-10-01 13:23:19 -0700

    first and last name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001010737) do
+ActiveRecord::Schema.define(version: 20171001184159) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 20171001010737) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
